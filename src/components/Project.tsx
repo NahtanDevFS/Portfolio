1708b170ba517b8974541c37ec362d417d0d851d
NahtanDevFS <jonathan04franco@gmail.com>
2026-01-10 00:24:57 -0600

    fix: some rendundant and unascalable code on projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,68 +1,89 @@
-'use client';
+"use client";
 
-import React from 'react'
-import './Project.css'
+import React, { useState } from "react";
+import "./Project.css";
 
 interface ProjectItemProps {
-  src_img: string;
+  images: string[];
   name: string;
   description: string;
   href_project: string;
   href_repo: string;
-  img_tech1: string;
-  img_tech2: string;
-  img_tech3: string;
-  img_tech4: string;
-  img_tech5: string;
-  img_tech6: string;
-  img_tech7: string;
+  technologies: Technology[];
 }
 
-const Project: React.FC<ProjectItemProps> = ({ 
-  src_img, 
-  name, 
-  description, 
+interface Technology {
+  name: string;
+  iconUrl: string;
+}
+
+const Project: React.FC<ProjectItemProps> = ({
+  images,
+  name,
+  description,
   href_project,
   href_repo,
-  img_tech1,
-  img_tech2,
-  img_tech3,
-  img_tech4,
-  img_tech5,
-  img_tech6,
-  img_tech7
- }) => {
+  technologies,
+}) => {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImg((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  const prevImage = () => {
+    setCurrentImg((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
 
   const handleButtonClick = (url: string) => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
 
   return (
-    <div className='projects-container'>
-      <div className='projects-content'>
-        <div className='projects-img-container'>
-          <img src={src_img} alt='profile' className='projects-image'/>
-        </div>
-        <div className='projects-desc-container'>
-          <h1 className='projects-desc-title'>{name}</h1>
-          <p className='projects-description'>{description}</p>
-          <div className='projects-tech-imgs-container'>
-            <img src={img_tech1} className='projects-tech-img'/>
-            <img src={img_tech2} className='projects-tech-img'/>
-            <img src={img_tech3} className='projects-tech-img'/>
-            <img src={img_tech4} className='projects-tech-img'/>
-            <img src={img_tech5} className='projects-tech-img'/>
-            <img src={img_tech6} className='projects-tech-img'/>
-            <img src={img_tech7} className='projects-tech-img'/>
+    <div className="projects-container">
+      <div className="projects-content">
+        <div className="projects-img-container">
+          <div className="projects-img-carousel">
+            <button onClick={prevImage}>{"<"}</button>
+            <img
+              src={images[currentImg]}
+              alt={`${name} screenshot`}
+              className="projects-image"
+            />
+            <button onClick={nextImage}>{">"}</button>
           </div>
-          <div className='projects-buttons-container'>
-            <button className='projects-button' onClick={() => handleButtonClick(href_project)}>View project</button>
-            <button className='projects-button' onClick={() => handleButtonClick(href_repo)}>View code</button>
+        </div>
+        <div className="projects-desc-container">
+          <h1 className="projects-desc-title">{name}</h1>
+          <p className="projects-description">{description}</p>
+          <div className="projects-tech-imgs-container">
+            {technologies.map((tech) => (
+              <img
+                key={tech.name}
+                src={tech.iconUrl}
+                alt={tech.name}
+                className="projects-tech-img"
+              />
+            ))}
+          </div>
+          <div className="projects-buttons-container">
+            <button
+              className="projects-button"
+              onClick={() => handleButtonClick(href_project)}
+            >
+              View project
+            </button>
+            <button
+              className="projects-button"
+              onClick={() => handleButtonClick(href_repo)}
+            >
+              View code
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Project;
---
+++ src/data/projectsData.ts
@@ -0,0 +1,24 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "Management system for a car repair shop business",
+    description:
+      "Development of a comprehensive platform for inventory and sales management in a real commercial environment for an auto parts workshop and sales company; I designed a complex inventory system segmented by product type (units, bulk/liquids), a real-time sales dashboard, robust authentication system and route protection for different users. All of the technologies that I used are: ",
+    images: ["taller_franco_dashboard.jpg", "taller_franco_inventario.jpg"],
+    href_project: "https://taller-franco.vercel.app/",
+    href_repo: "https://github.com/NahtanDevFS/taller_franco.git",
+    technologies: [
+      { name: "HTML", iconUrl: "html.png" },
+      { name: "CSS", iconUrl: "css.png" },
+      {
+        name: "TYPESCRIPT",
+        iconUrl:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png",
+      },
+      { name: "NEXT.JS", iconUrl: "nextjs.png" },
+      { name: "POSTGRESQL", iconUrl: "postgresql.png" },
+      { name: "SUPABASE", iconUrl: "supabase.webp" },
+      { name: "GIT", iconUrl: "git.png" },
+    ],
+  },
+];
